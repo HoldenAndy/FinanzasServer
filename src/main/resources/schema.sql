da159ea8ac951c4cb71cfb1e5517266cfaf6d0c9
HoldenAndy <kaziroz09@gmail.com>
2025-12-29 01:45:46 -0500

    Implementacion de campos categorias y movimientos

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,24 @@ CREATE TABLE IF NOT EXISTS usuarios (
     activado BOOLEAN DEFAULT FALSE,
     codigo_verificacion VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS categorias (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL, -- Ej: Comida, Transporte, Sueldo
+    tipo VARCHAR(20) NOT NULL, -- INGRESO o EGRESO
+    usuario_id BIGINT NOT NULL, -- Cada usuario crea sus propias categorías
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE IF NOT EXISTS movimientos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id BIGINT NOT NULL,
+    categoria_id BIGINT NOT NULL,
+    monto DECIMAL(19, 2) NOT NULL,
+    fecha DATE NOT NULL,
+    descripcion VARCHAR(255),
+    tipo VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
